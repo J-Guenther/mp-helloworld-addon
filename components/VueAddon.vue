98da90f4e28aabe2d808f0f71067e40704ae8cc7
Jannik Günther <jannik.guenther@lgln.niedersachsen.de>
2025-03-17 12:27:06 +0100

    initial commit

---
+++ components/VueAddon.vue
@@ -0,0 +1,51 @@
+// myMasterPortalFolder/addons/VueAddon/components/VueAddon.vue
+
+<script>
+import Tool from "../../../src/modules/tools/Tool.vue";
+import {mapGetters, mapActions, mapMutations} from "vuex";
+import getters from "../store/gettersVueAddon";
+import mutations from "../store/mutationsVueAddon";
+
+export default {
+  name: "VueAddon",
+  components: {
+    Tool
+  },
+  computed: {
+    ...mapGetters("Tools/VueAddon", Object.keys(getters))
+  },
+  /**
+   * Put initialize here if mounting occurs after config parsing
+   * @returns {void}
+   */
+  mounted () {
+    this.initialize();
+  },
+  methods: {
+    ...mapActions("Tools/VueAddon", [
+      "initialize"
+    ]),
+    ...mapMutations("Tools/VueAddon", Object.keys(mutations))
+  }
+};
+</script>
+
+<template lang="html">
+  <Tool
+      :title="$t(name)"
+      :icon="icon"
+      :active="active"
+      :render-to-window="renderToWindow"
+      :resizable-window="resizableWindow"
+      :deactivateGFI="deactivateGFI"
+  >
+    <template v-slot:toolBody>
+      <div
+          v-if="active"
+          id="vue-addon"
+      >
+        {{ $t("additional:modules.tools.VueAddon.content") }}
+      </div>
+    </template>
+  </Tool>
+</template>
---
+++ index.js
@@ -0,0 +1,15 @@
+// myMasterPortalFolder/addons/VueAddon/index.js
+
+import VueAddonComponent from "./components/VueAddon.vue";
+import VueAddonStore from "./store/VueAddon";
+import deLocale from "./locales/de/additional.json";
+import enLocale from "./locales/en/additional.json";
+
+export default {
+    component: VueAddonComponent,
+    store: VueAddonStore,
+    locales: {
+        de: deLocale,
+        en: enLocale
+    }
+};
